ef5960f9a26f7282aaf47a545ca4955728c6f31c
vithin-k <124872486+vithin-k@users.noreply.github.com>
2023-06-08 10:06:16 +0530

    fist commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+
+
+// screnn navigation button
+
+  window.addEventListener('scroll', function() {
+    var scrollButton = document.getElementById('scrollButton');
+    if (window.scrollY === 0) {
+      scrollButton.style.display = 'none';
+    } else {
+      scrollButton.style.display = 'block';
+      document.querySelector('.scroll-button').addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior:"smooth" });
+      });
+    }
+  });
